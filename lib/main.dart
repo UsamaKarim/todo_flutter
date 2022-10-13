e1af8af45c0f361183551c335b15840ef2a2910b
Usama Karim <sonuosama@gmail.com>
2022-10-13 20:37:13 +0500

    feat: :sparkles: Implement test ads using AdMob

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,12 @@ import 'package:dribble_ui/pages/home_page.dart';
 import 'package:dribble_ui/state/todo_provider.dart';
 import 'package:dribble_ui/utils/contants.dart';
 import 'package:flutter/material.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'package:provider/provider.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await MobileAds.instance.initialize();
   runApp(const MyApp());
 }
 
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:dribble_ui/data/ad_helper.dart';
 import 'package:dribble_ui/models/todo.dart';
 import 'package:dribble_ui/models/todo_item.dart';
 import 'package:dribble_ui/state/todo_provider.dart';
@@ -5,8 +6,12 @@ import 'package:dribble_ui/utils/contants.dart';
 import 'package:dribble_ui/widgets/todo_item_widget.dart';
 import 'package:dribble_ui/widgets/todo_text_field.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'package:provider/provider.dart';
 import 'package:uuid/uuid.dart';
+
+const _maxTriesUponAdLoadingFailed = 3;
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -16,17 +21,128 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  late BannerAd _bottomBannerAd;
+  late BannerAd _inlineBannerAd;
+  InterstitialAd? _interstitialAd;
+
+  bool _isBottomAdLoaded = false;
+  bool _isInlineAdLoaded = false;
+
   final controller = TextEditingController();
+
+  final _inlineAdIndex = 2;
+  int _interstitialAdLoadAttempts = 0;
+
+  Future<void> createBottomBannerAd() async {
+    _bottomBannerAd = BannerAd(
+      size: AdSize.banner,
+      adUnitId: AdHelper.bannerAdUnitId,
+      listener: BannerAdListener(
+        onAdLoaded: (ad) => setState(() {
+          _isBottomAdLoaded = true;
+        }),
+        onAdFailedToLoad: (ad, error) => ad.dispose(),
+      ),
+      request: const AdRequest(),
+    );
+    await _bottomBannerAd.load();
+  }
+
+  Future<void> createInlineBannerAd() async {
+    _inlineBannerAd = BannerAd(
+      size: AdSize.mediumRectangle,
+      adUnitId: AdHelper.bannerAdUnitId,
+      listener: BannerAdListener(
+        onAdLoaded: (ad) => setState(() {
+          _isInlineAdLoaded = true;
+        }),
+        onAdFailedToLoad: (ad, error) => ad.dispose(),
+      ),
+      request: const AdRequest(),
+    );
+    await _inlineBannerAd.load();
+  }
+
+  int getInlineAdDifferenciateIndex(int index) {
+    if (_isInlineAdLoaded && index >= _inlineAdIndex) {
+      return index - 1;
+    }
+    return index;
+  }
+
+  Future<void> createInterstitialAd() async {
+    InterstitialAd.load(
+      adUnitId: AdHelper.interstitialAdUnitId,
+      request: const AdRequest(),
+      adLoadCallback: InterstitialAdLoadCallback(
+        onAdLoaded: ((ad) {
+          _interstitialAd = ad;
+          _interstitialAdLoadAttempts = 0;
+          // throw LoadAdError(code, domain, message, responseInfo)
+        }),
+        onAdFailedToLoad: ((error) {
+          _interstitialAd = null;
+          _interstitialAdLoadAttempts += 1;
+          if (_interstitialAdLoadAttempts <= _maxTriesUponAdLoadingFailed) {
+            createInterstitialAd();
+          }
+        }),
+      ),
+    );
+  }
+
+  Future<void> showInterstitialAd() async {
+    if (_interstitialAd != null) {
+      await SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual,
+          overlays: []);
+      _interstitialAd!.fullScreenContentCallback = FullScreenContentCallback(
+        onAdDismissedFullScreenContent: (ad) async {
+          await ad.dispose();
+          await createInterstitialAd();
+          await SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual,
+              overlays: SystemUiOverlay.values);
+        },
+        onAdFailedToShowFullScreenContent: (ad, error) async {
+          await ad.dispose();
+          await createInterstitialAd();
+          await SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual,
+              overlays: SystemUiOverlay.values);
+        },
+      );
+      await _interstitialAd!.show();
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    createBottomBannerAd();
+    createInlineBannerAd();
+    createInterstitialAd();
+  }
 
   @override
   void dispose() {
     controller.dispose();
+    _bottomBannerAd.dispose();
+    _inlineBannerAd.dispose();
+    _interstitialAd?.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      persistentFooterButtons: [
+        if (_isBottomAdLoaded)
+          Center(
+            child: SizedBox(
+              height: _bottomBannerAd.size.height.toDouble(),
+              width: _bottomBannerAd.size.width.toDouble(),
+              child: AdWidget(ad: _bottomBannerAd),
+            ),
+          )
+      ],
       appBar: AppBar(
         backgroundColor: Colors.white,
         elevation: 0.5,
@@ -76,10 +192,12 @@ class _HomePageState extends State<HomePage> {
             child: Consumer<TodoProvider>(
               builder: (context, todo, child) {
                 return ListView.builder(
-                  itemCount: todo.listOfTodos.length + 1,
+                  itemCount:
+                      todo.listOfTodos.length + 1 + (_isInlineAdLoaded ? 1 : 0),
                   padding: const EdgeInsets.symmetric(horizontal: 8),
                   itemBuilder: ((context, index) {
-                    if (index == todo.listOfTodos.length) {
+                    if (index ==
+                        todo.listOfTodos.length + (_isInlineAdLoaded ? 1 : 0)) {
                       return Padding(
                         padding: const EdgeInsets.all(16.0),
                         child: TodoTextField(
@@ -89,10 +207,18 @@ class _HomePageState extends State<HomePage> {
                         ),
                       );
                     }
+                    if (_isInlineAdLoaded && _inlineAdIndex == index) {
+                      return SizedBox(
+                        width: _inlineBannerAd.size.width.toDouble(),
+                        height: _inlineBannerAd.size.height.toDouble(),
+                        child: AdWidget(ad: _inlineBannerAd),
+                      );
+                    }
                     final color = randomColors[index % randomColors.length];
                     return TodoItemWidget(
                       barColor: color,
-                      todo: todo.listOfTodos[index],
+                      todo: todo
+                          .listOfTodos[getInlineAdDifferenciateIndex(index)],
                     );
                   }),
                 );
@@ -108,17 +234,20 @@ class _HomePageState extends State<HomePage> {
     );
   }
 
-  void addTodo() {
-    if (controller.text.isNotEmpty) {
-      final todo = Todo(
-        todoItem: TodoItem(
-          id: const Uuid().v4(),
-          name: controller.text,
-        ),
-      );
-      context.read<TodoProvider>().addTodo(todo);
-      controller.text = '';
-      FocusScope.of(context).unfocus();
+  Future<void> addTodo() async {
+    await showInterstitialAd();
+    if (mounted) {
+      if (controller.text.isNotEmpty) {
+        final todo = Todo(
+          todoItem: TodoItem(
+            id: const Uuid().v4(),
+            name: controller.text,
+          ),
+        );
+        context.read<TodoProvider>().addTodo(todo);
+        controller.text = '';
+        FocusScope.of(context).unfocus();
+      }
     }
   }
 }
